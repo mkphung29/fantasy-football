2d220c891f9bea251623cce95e0d599de854fb00
Madison Phung <maddykay396@gmail.com>
2024-01-02 22:46:33 -0600

    reconfigure build

--- build.py
+++ build.py
@@ -1,12 +1,6 @@
 from flask_frozen import Freezer
 from __init__ import create_app
 
-# Monkey-patch Flask to work with Frozen-Flask
-import flask
-from werkzeug.wrappers import Response
-
-flask.Flask.response_class = Response
-
 # Call the application factory function to construct a Flask application
 # instance using the development configuration
 app = create_app()
